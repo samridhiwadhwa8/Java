bc177e26b76095ecbea511d7e0bf5bf7e505f666
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-04-01 12:13:34 +0530

    Implement bottom-up approach for Fibonacci in leetcode509
    Added a bottom-up approach for Fibonacci calculation.

--- dp/leetcode509.java
+++ dp/leetcode509.java
@@ -4,11 +4,21 @@ class Solution {
         Arrays.fill(dp,-1);
         return sol(dp,n);
     }
+    //MEMO
     public int sol(int[] dp, int n){
         if(n<=1){
             return n;
         }
         if(dp[n]!=-1)return dp[n];
         return dp[n]= sol(dp,n-1)+ sol(dp,n-2);
+    }
+    //BOTTOM UP APPROACH
+    public int sol(int[] dp, int n){
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
