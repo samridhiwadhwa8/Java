cdbe7bf837629529e1015fe8ec7dff4cabdaa278
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-04-12 16:45:07 +0530

    Update leetcode1092.java

--- dp/leetcode1092.java
+++ dp/leetcode1092.java
@@ -52,3 +52,67 @@ class Solution {
     return res.reverse().toString();
 }
 }
+//2nd approach
+class Solution {
+    int[][] dp;
+    public String shortestCommonSupersequence(String str1, String str2) {
+        int n=str1.length();
+        int m=str2.length();
+        dp=new int[n][m];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        lcs(n-1,m-1,str1,str2);
+        String a=lc(n-1,m-1,str1,str2);
+        int k=0;
+        int i=0,j=0;
+        StringBuilder r=new StringBuilder();
+        while(k< a.length()){
+            while(i<str1.length() && str1.charAt(i)!=a.charAt(k)){
+                r.append(str1.charAt(i));
+                i++;
+            }
+            while(j<str2.length() && str2.charAt(j)!=a.charAt(k)){
+                r.append(str2.charAt(j));
+                j++;
+            }
+            r.append(str1.charAt(i));
+            i++;
+            j++;
+            k++;
+        }
+        while(i<str1.length()){
+            r.append(str1.charAt(i));
+            i++;
+        }
+        while(j<str2.length()){
+            r.append(str2.charAt(j));
+            j++;
+        }
+        return r.toString();
+    }
+    public int lcs(int i,int j,String a ,String b){
+        if(i<0 || j<0)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(a.charAt(i)==b.charAt(j))dp[i][j]= 1+ lcs(i-1,j-1,a,b);
+        else dp[i][j]= Math.max(lcs(i,j-1,a,b),lcs(i-1,j,a,b));
+        return dp[i][j];
+    }
+    public String lc(int i,int j,String a,String b){
+        StringBuilder res=new StringBuilder();
+        while(i>=0 && j>=0){
+            if(a.charAt(i)==b.charAt(j)){
+                res.append(a.charAt(i));
+                i--;
+                j--;
+            }else{
+                if(i>0 && dp[i-1][j]>= (j>0 ? dp[i][j-1]: 0)){
+                    i--;
+                }else{
+                    j--;
+                }
+            }
+        }
+        return res.reverse().toString();
+    }
+}
