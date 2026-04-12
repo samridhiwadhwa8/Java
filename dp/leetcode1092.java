5462ed83bf2780a3247bbfbc6df458514558eca4
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-04-12 16:09:49 +0530

    Implement shortest common supersequence algorithm

---
+++ dp/leetcode1092.java
@@ -0,0 +1,54 @@
+class Solution {
+    int[][] dp;
+    public String shortestCommonSupersequence(String str1, String str2) {
+        int n=str1.length();
+        int m=str2.length();
+         dp=new int[n][m];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        lcs(n-1,m-1,str1,str2);
+        return scs(n-1,m-1,str1,str2);
+    }
+    public int lcs(int i,int j,String s,String r){
+        if(i<0 || j<0)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s.charAt(i)==r.charAt(j)){
+            dp[i][j]=1+ lcs(i-1,j-1,s,r);
+        }else{
+            dp[i][j]=Math.max(lcs(i-1,j,s,r),lcs(i,j-1,s,r));
+        }
+        return dp[i][j];
+    }
+    public String scs(int i,int j,String s,String r){
+        
+    StringBuilder res=new StringBuilder();
+        while(i>=0 && j>=0){
+        if(s.charAt(i)==r.charAt(j)){
+            res.append(s.charAt(i));
+            i--;
+            j--;
+        }
+        else{
+        if(i> 0 && dp[i-1][j] >=(j>0 ? dp[i][j-1]: 0)){
+            res.append(s.charAt(i));
+            i--;
+        }else{
+            res.append(r.charAt(j));
+            j--;
+        }
+        }
+    }
+    
+        while(i>=0){
+            res.append( s.charAt(i));
+            i--;
+        }
+        while(j>=0){
+           res.append(r.charAt(j));
+            j--;
+        }
+    // if(res.size() == s.length()+r.length() - (lcs(s.length()-1,r.length()-1,s,r)))
+    return res.reverse().toString();
+}
+}
