19449e4c40f1535834857e5eae05be0b35c73a8d
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-04-17 09:41:43 +0530

    Implement coin toss simulation using recursion

---
+++ recursion/cointoss.java
@@ -0,0 +1,16 @@
+    class Main{
+        public static void main(String[] args)
+        {
+           Scanner sc=new Scanner(System.in);
+           int n=sc.nextInt();
+           toss(n,"");
+        }
+        public static void toss(int n,String ans){
+            if(n==0){
+                System.out.print(ans+" ");
+                return ;
+            }
+            toss(n-1,ans+"H");
+            toss(n-1,ans+"T");
+        }
+    }
