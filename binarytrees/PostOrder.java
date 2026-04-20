97973537736e33a8044de34217fe0c716703b640
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-04-20 21:48:06 +0530

    Update PostOrder.java

--- binarytrees/PostOrder.java
+++ binarytrees/PostOrder.java
@@ -1,3 +1,4 @@
+//DFS Approach
 class Solution {
     List<Integer> res=new ArrayList<>();
     public List<Integer> postorder(TreeNode root) {
@@ -10,4 +11,24 @@ class Solution {
         post(root.right);
         res.add(root.data);
     }
+}
+//Iterative Approach
+class Solution {
+    List<Integer> res=new ArrayList<>();
+    public List<Integer> postorder(TreeNode root) {
+    Stack<TreeNode> st=new Stack<>();
+    Stack<TreeNode> st2=new Stack<>();
+    st.push(root);
+    while(!st.isEmpty()){
+        TreeNode curr=st.pop();
+        st2.push(curr);
+        if(curr.left!=null)st.push(curr.left);
+        if(curr.right!=null)st.push(curr.right);
+    }
+    while(!st2.isEmpty()){
+        TreeNode r=st2.pop();
+        res.add(r.data);
+    }
+    return res;
+}
 }
