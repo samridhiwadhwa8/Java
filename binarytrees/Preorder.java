fbb09284babf725ad595587afcbd8b568c6fb5dc
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-04-20 21:31:45 +0530

    Update Preorder.java

--- binarytrees/Preorder.java
+++ binarytrees/Preorder.java
@@ -1,4 +1,5 @@
 class Solution {
+       //DFS
        List<Integer> res=new ArrayList<>();
     public List<Integer> preorder(TreeNode root) {
        pre(root);
@@ -9,5 +10,25 @@ class Solution {
         res.add(root.data);
         pre(root.left);
         pre(root.right);
+    }
+}
+// Iterative Approach
+class Solution {
+       List<Integer> res=new ArrayList<>();
+    public List<Integer> preorder(TreeNode root) {
+    if(root==null)return res;
+    Stack<TreeNode> st=new Stack<>();
+    st.push(root);
+    while(!st.isEmpty()){
+        TreeNode curr=st.pop();
+        res.add(curr.data);
+        if(curr.right!=null){
+            st.push(curr.right);
+        }
+        if(curr.left!=null){
+            st.push(curr.left);
+        }
+    }
+       return res;
     }
 }
