e68fee32e7f36ab8f92294a70fafb73aa416de27
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-04-22 22:36:33 +0530

    Update SymetricTree.java

--- binarytrees/SymetricTree.java
+++ binarytrees/SymetricTree.java
@@ -32,3 +32,17 @@ class Solution {
         return t1.val==t2.val && isSame(t1.left,t2.left) && isSame(t1.right,t2.right);
     }
 }
+//2nd approach
+
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null)return true;
+        return mirror(root.left,root.right);
+    }
+public boolean mirror(TreeNode t1,TreeNode t2){
+        if(t1==null && t2==null)return true;
+        if(t1==null||t2==null)return false;
+        return t1.val==t2.val && mirror(t1.left,t2.right) && mirror(t1.right,t2.left);
+    }
+}
