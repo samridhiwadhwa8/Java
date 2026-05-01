c1e5c5cf4dd41cda93e529b3f10a0d7f30556995
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-05-01 13:17:52 +0530

    Update BalancedTree.java

--- binarytrees/BalancedTree.java
+++ binarytrees/BalancedTree.java
@@ -28,3 +28,15 @@ class Solution {
        return 1+Math.max(left,right);
     }
 }
+//OR
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null)return true;
+        if(Math.abs(height(root.left)-height(root.right))>1)return false;
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+    public int height(TreeNode root){
+        if(root==null)return 0;
+        return 1+Math.max(height(root.left),height(root.right));
+    }
+}
