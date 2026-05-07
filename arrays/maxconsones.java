e16564f2838cca94405168fe2bae9a66f25de98d
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-05-07 13:04:08 +0530

    Update maxconsones.java

--- arrays/maxconsones.java
+++ arrays/maxconsones.java
@@ -12,3 +12,20 @@ class Solution {
         return max;
     }
 }
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int left=0;
+        int right=0;
+        int max=0;
+        while(right<nums.length){
+            if(nums[right]==0){
+                left=right+1;
+            }
+            if(nums[right]==1){
+            max=Math.max(max,right-left+1);
+            }
+            right++;
+        }
+        return max;
+    }
+}
