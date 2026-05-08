8e4d4ba0b9d21461e6962fd7fcf0f7dee0448363
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-05-08 16:32:46 +0530

    Create fourSum.java

---
+++ arrays/fourSum.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> re=new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            for(int j=i+1;j<nums.length;j++){
+                if(j>i+1 && nums[j]==nums[j-1])continue;
+                int k=j+1,l=nums.length-1;
+                while(k<l){
+                    long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum<target){
+                        k++;
+                    }
+                    else if(sum>target){
+                        l--;
+                    }
+                    else{
+                        List<Integer> res=new ArrayList<>();
+                        res.add(nums[i]);
+                        res.add(nums[j]);
+                        res.add(nums[k]);
+                        res.add(nums[l]);
+                        
+                    re.add(new ArrayList<>(res));
+                        k++;
+                        l--; 
+                        while(k<l && nums[k]==nums[k-1])k++;
+                        while(k<l && nums[l]==nums[l+1])l--;
+                    }
+                }
+            }
+        }
+        return re;
+    }
+}
