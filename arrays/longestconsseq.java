2adac2e6a9bddc07d9d020bfd536993108d5c7d1
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-05-08 09:35:36 +0530

    Update longestconsseq.java

--- arrays/longestconsseq.java
+++ arrays/longestconsseq.java
@@ -1,12 +1,13 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
+        if(nums.length==0)return 0;
         Arrays.sort(nums);
-        int c=1,max=0;
+        int c=1,max=1;
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]==nums[i+1])continue;
             if(nums[i]+1==nums[i+1]){
                 c++;
-                max=Math.max(c,max);
+                   max=Math.max(c,max);
             }else{
                 c=1;
             }
