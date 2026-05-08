b47e52880758d376de3ceff5e39b36255b232ebd
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-05-08 16:51:14 +0530

    Update mergesortedarray.java

--- arrays/mergesortedarray.java
+++ arrays/mergesortedarray.java
@@ -26,3 +26,20 @@ class Solution {
         }
     }
 }
+//without extra space
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=m-1,j=n-1,s=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[s--]=nums1[i--];
+            }
+            else{
+                nums1[s--]=nums2[j--];
+            }
+        }
+        while(j>=0){
+            nums1[s--]=nums2[j--];
+        }
+    }
+}
