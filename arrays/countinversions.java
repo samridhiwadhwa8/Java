e535094a05b8bbbef69a77718fa672de2fa0edb7
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-05-09 18:49:11 +0530

    Create countinversions.java

---
+++ arrays/countinversions.java
@@ -0,0 +1,13 @@
+//Brute force
+class Solution {
+    public long numberOfInversions(int[] nums) {
+        long count=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]>nums[j] && i<j)count++;
+            }
+        }
+        return count;
+    }
+}
+//optimal approach -> merge sort
