148f40db5e7660c8fe5af252b76413f156cfb592
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-05-15 23:13:36 +0530

    Create fruitsin.java

---
+++ arrays/fruitsin.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        // if(fruits.length==1 || fruits.length==2)return fruits.length;
+        int left=0;
+        int right=0;
+        int max=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        while(right<fruits.length){
+            map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+            while(map.size()>2){
+                int l=map.get(fruits[left]);
+                map.put(fruits[left],map.getOrDefault(fruits[left],l)-1);
+                if(map.get(fruits[left])==0){
+                    map.remove(fruits[left]);
+                }
+                left++;
+            }
+                max=Math.max(max,right-left+1);
+            right++;
+        }
+        return max;
+    }
+}
