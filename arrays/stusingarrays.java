89215d31073465f68b2c57d15b1c855fc54947d8
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-06-27 23:18:09 +0530

    Create stusingarrays.java

---
+++ arrays/stusingarrays.java
@@ -0,0 +1,32 @@
+class myStack {
+    int top;
+    int[] arr;
+    int n;
+    public myStack(int n) {
+        this.n=n;
+        arr=new int[n];
+        top=-1;
+    }
+
+    public boolean isEmpty() {
+       return top==-1;
+    }
+
+    public boolean isFull() {
+      return top==n-1;
+    }
+
+    public void push(int x) {
+        if(top==n-1)return ;
+        arr[++top]=x;
+    }
+
+    public void pop() {
+       if(top==-1)return ;
+       top--;
+    }
+
+    public int peek() {
+        return arr[top];
+    }
+}
