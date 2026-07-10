b86b4f0533827d3e0453d33bebc191f9ea08ccb9
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-10 16:56:59 +0530

    Leetcode: 😇 Actually Good Hints

---
+++ leetcode/count-subarrays-with-score-less-than-k-😇-actually-good-hints.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long countSubarrays(int[] nums, 
+    long k) {
+        long sum=0;
+        int right=0;
+        int left=0;
+        long c=0;
+        while(right<nums.length){
+            sum+=nums[right];
+        }
+            while(sum*(right-left+1)>=k){
+                sum-=nums[left];
+            }
+    }
+                left++;
+            right++;
+        return c;
+            c+=right-left+1;
+}
