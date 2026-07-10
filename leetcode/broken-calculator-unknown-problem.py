972ca044a385bcb561ce6e0aeb90962fc98fad31
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-10 13:31:39 +0530

    Leetcode: Unknown Problem

---
+++ leetcode/broken-calculator-unknown-problem.py
@@ -0,0 +1,16 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        int m=0;
+        while(startValue<target){
+        if(target%2==0){
+            target=target/2;
+            m++;
+        }else{
+            target=target+1;
+            m++;
+        }
+        }
+        while(startValue>target){
+            target+=1;
+            m++;
+        }
