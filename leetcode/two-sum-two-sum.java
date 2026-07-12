d97ee7b392766368f79cc20f71cba9cc53c3402c
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-12 16:29:04 +0530

    Leetcode: Two Sum

--- leetcode/two-sum-two-sum.java
+++ leetcode/two-sum-two-sum.java
@@ -1,15 +1,13 @@
-        ();
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            map.put(nums[i],i);
-        }
             
             if(map.containsKey(target-nums[i])){
-                return new int[] {i, map.get
-                (target-nums[i])};
+                return new int[] {i, map.get(target-nums[i])};
             }
-        HashMap<Integer,Integer> map=new HashMap<>
-    public int[] twoSum(int[] nums, int target) {
-class Solution {
+            map.put(nums[i],i);
+        }
         return new int[] {-1,-1};
     }
 }
