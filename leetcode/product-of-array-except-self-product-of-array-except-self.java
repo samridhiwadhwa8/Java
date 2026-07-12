9f47a3037e0a9661587ea91cb13d828a87360db6
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-12 16:55:14 +0530

    Leetcode: Product of Array Except Self

---
+++ leetcode/product-of-array-except-self-product-of-array-except-self.java
@@ -0,0 +1,14 @@
+            suffix[i]=suffix[i+1]*nums[i+1];
+        }
+        for(int i=0;i<ans.length;i++){
+            ans[i]=suffix[i]*pref[i];
+        }
+    }
+        return ans;
+        for(int i=nums.length-2;i>=0;i--){
+        int j=1;
+        suffix[nums.length-1]=1;
+        }
+            pref[i]=pref[i-1]*nums[i-1];
+        for(int i=1;i<nums.length;i++){
+}
