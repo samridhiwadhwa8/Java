f20b36a47b55eea3c28d6b479e1cc8923d98dfdb
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-12 16:35:36 +0530

    Leetcode: Best Time to Buy and Sell Stock

---
+++ leetcode/best-time-to-buy-and-sell-stock-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,15 @@
+            if(prices[i]<min){
+                min=prices[i];
+            }
+            else if(prices[i]-min>maxprofit){
+                maxprofit=Math.max(maxprofit,prices[i]-min);
+            }
+    }
+        for(int i=0;i<prices.length;i++){
+        int min=Integer.MAX_VALUE;
+        int maxprofit=0;
+    public int maxProfit(int[] prices) {
+class Solution {
+    return maxprofit;
+}
+}
