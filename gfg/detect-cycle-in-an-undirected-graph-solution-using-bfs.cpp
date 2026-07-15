9c03156ee5281c051470ce3dfdc839503873131f
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-15 12:56:17 +0530

    Gfg: solution using BFS

--- gfg/detect-cycle-in-an-undirected-graph-solution-using-bfs.cpp
+++ gfg/detect-cycle-in-an-undirected-graph-solution-using-bfs.cpp
@@ -1,3 +1,11 @@
+class Pair{
+    int node;
+    int parent;
+    Pair(int node,int parent){
+        this.node=node;
+        this.parent=parent;
+    }
+}
 class Solution {
     public boolean isCycle(int V, int[][] edges) {
         HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
@@ -12,25 +20,19 @@ class Solution {
         boolean[] visited=new boolean[V];
         for(int i=0;i<V;i++){
             if(!visited[i]){
-            if(dfs(map,visited,i,-1))return true;
+            if(bfs(map,visited,i,-1))return true;
             }
         }
         return false;
-        
         // return dfs(map,new boolean[map.size()],0,-1);
-        
     }
-    public boolean dfs(HashMap<Integer,ArrayList<Integer>> map,boolean[] visited,int start,int parent){
-        visited[start]=true;
-        if(map.containsKey(start)){
-        for(int v:map.get(start)){
-            if(v==parent)continue;
-            if(visited[v]==true)return true;
-            if(dfs(map,visited,v,start)){
-                return true;
-            }
-        }
-        }
-        return false;
-    }
-}
+    public boolean bfs(HashMap<Integer,ArrayList<Integer>> map,boolean[] visited,int start,int parent){
+       Queue<Pair> q=new LinkedList<>();
+       visited[start]=true;
+       q.add(new Pair(start,-1));
+       while(!q.isEmpty()){
+           Pair curr=q.poll();
+           int n=curr.node;
+           int p=curr.parent;
+           if(!map.containsKey(n)){
+           continue;
