28cd4407dc7754aa6e0154163c1cfd6a1ef47481
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-16 14:09:04 +0530

    Implement BFS to detect cycles in a directed graph

--- gfg/detect-cycle-in-a-directed-graph-expected-complexities.cpp
+++ gfg/detect-cycle-in-a-directed-graph-expected-complexities.cpp
@@ -34,4 +34,54 @@ class Solution {
         return false;
         
     }
+//bfs solution
+/ class Pair{
+//     int node;
+//     int parent;
+//     Pair(int node,int parent){
+//         this.node=node;
+//         this.parent=parent;
+// }
+// }
+class Solution {
+    public boolean isCyclic(int V, int[][] edges) {
+        HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
+        for(int i=0;i<edges.length;i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            map.putIfAbsent(u,new ArrayList<>());
+            map.putIfAbsent(v,new ArrayList<>());
+            map.get(u).add(v);
+        }
+        int[] indegree=new int[V];
+        for(int i=0;i<V;i++){
+            if(map.containsKey(i)){
+                for(int v:map.get(i)){
+                    indegree[v]++;
+                }
+            }
+        }
+        ArrayList<Integer> ans=new ArrayList<>();
+        bfs(map,indegree,V,ans);
+       return ans.size()!=V;
+    }
+    public void bfs(HashMap<Integer,ArrayList<Integer>> map,int[] indegree,int V,ArrayList<Integer> ans){
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if (indegree[i]==0){
+                q.add(i);
+            }
+        }
+        while(!q.isEmpty()){
+            int node=q.poll();
+            ans.add(node);
+            if(map.containsKey(node)){
+                for(int v:map.get(node)){
+                    indegree[v]--;
+                if(indegree[v]==0)q.add(v);
+                }
+            }
+        }
+    }
+    }
 }
