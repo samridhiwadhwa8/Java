396295fe97ae622e53b47d20be21c2e8554e876d
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-16 21:29:06 +0530

    Update course-schedule-course-schedule.java

--- leetcode/course-schedule-course-schedule.java
+++ leetcode/course-schedule-course-schedule.java
@@ -1,29 +1,42 @@
-            }
-        }
-        ArrayList<Integer> ans=new ArrayList<>();
-        bfs(map,numCourses,indegree,ans);
-        return ans.size()==numCourses;
-    }
-    public void bfs(HashMap<Integer,ArrayList<Integer>> map,int nc,int[] indegree,List<Integer> ans){
-        Queue<Integer> q=new LinkedList<>();
-        for(int i=0;i<nc;i++){
-            if(indegree[i]==0){
-                q.add(i);
-            }
-                }
-                for(int v:map.get(i)){
-                    indegree[v]++;
-        for(int i=0;i<numCourses;i++){
-            if(map.containsKey(i)){
-        
-        int[] indegree=new int[numCourses];
-                map.putIfAbsent(v,new ArrayList<>());
-                map.get(u).add(v);
-            }
-                map.putIfAbsent(u,new ArrayList<>());
-                int u=prerequisites[i][0];
-                int v=prerequisites[i][1];
-        HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
-        for(int i=0;i<prerequisites.length;i++){
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-class Solution {
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
+        for(int i=0;i<prerequisites.length;i++){
+                int u=prerequisites[i][0];
+                int v=prerequisites[i][1];
+                map.putIfAbsent(u,new ArrayList<>());
+                map.putIfAbsent(v,new ArrayList<>());
+                map.get(u).add(v);
+            }
+        
+        int[] indegree=new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            if(map.containsKey(i)){
+                for(int v:map.get(i)){
+                    indegree[v]++;
+                }
+            }
+        }
+        ArrayList<Integer> ans=new ArrayList<>();
+        bfs(map,numCourses,indegree,ans);
+        return ans.size()==numCourses;
+    }
+    public void bfs(HashMap<Integer,ArrayList<Integer>> map,int nc,int[] indegree,List<Integer> ans){
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<nc;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        while(!q.isEmpty()){
+            int node=q.poll();
+            ans.add(node);
+            if(map.containsKey(node)){
+                for(int v:map.get(node)){
+                    indegree[v]--;
+                    if(indegree[v]==0)q.add(v);
+                }
+            }
+        }
+    }
+}
