9d3e5a47ceb420656c5e2394cee2418a3246c2ce
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-16 14:02:07 +0530

    Gfg: Expected Complexities

--- gfg/topological-sort-expected-complexities.cpp
+++ gfg/topological-sort-expected-complexities.cpp
@@ -8,30 +8,31 @@ for(int i=0;i<edges.length;i++){
     map.putIfAbsent(v,new ArrayList<>());
     map.get(u).add(v);
 }
-Stack<Integer> st=new Stack<>();
-boolean[] visited=new boolean[V];
+int[] indegree=new int[V];
 for(int i=0;i<V;i++){
-    if(!visited[i]){
-        dfs(map,i,st,visited);
-    }
-}
-ArrayList<Integer> res=new ArrayList<>();
-while(!st.isEmpty()){
-    res.add(st.pop());
-}
-return res;
-}
-public void dfs(HashMap<Integer,ArrayList<Integer>> map,int start,Stack<Integer> st,boolean[] visited){
-    // st.push(start);
-    visited[start]=true;
-    if(map.containsKey(start)){
-    for(int v:map.get(start)){
-        if(!visited[v]){
-            dfs(map,v,st,visited);
-            // visited[v]=true;
-        }
-    }
-    }
-    st.push(start);
+    if(map.containsKey(i)){
+for(int j:map.get(i)){
+    indegree[j]++;
 }
 }
+}
+ArrayList<Integer> ans=new ArrayList<>();
+bfs(map,indegree,ans,V);
+return ans;
+}
+
+// Stack<Integer> st=new Stack<>();
+// boolean[] visited=new boolean[V];
+// for(int i=0;i<V;i++){
+//     if(!visited[i]){
+//         dfs(map,i,st,visited);
+//     }
+// }
+// ArrayList<Integer> res=new ArrayList<>();
+// while(!st.isEmpty()){
+//     res.add(st.pop());
+// }
+// return res;
+public void bfs(HashMap<Integer,ArrayList<Integer>> map,int[] indegree,ArrayList<Integer> ans,int V){
+    Queue<Integer> q=new LinkedList<>();
+    for(int i=0;i<V;i++){
