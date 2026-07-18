30952604b0e7f456928fc8ff0c4de52cc0398d9e
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-18 11:17:49 +0530

    Leetcode: Two Sum II

---
+++ leetcode/two-sum-ii-input-array-is-sorted-two-sum-ii.java
@@ -0,0 +1,13 @@
+                return new int[]{left+1,right+1};
+            }
+            else if(sum>target){
+                right--;
+            }
+            else{
+            int sum=numbers[left]+numbers[right];
+            if(sum==target){
+        while(left<right){
+        int left=0;
+        int right=numbers.length-1;
+    public int[] twoSum(int[] numbers, int target) {
+class Solution {
