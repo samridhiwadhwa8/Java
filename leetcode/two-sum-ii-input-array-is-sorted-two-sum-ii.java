eaa531484aada771eda817046fc6d92d5a6c4f18
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-18 11:17:55 +0530

    Leetcode: Two Sum II

--- leetcode/two-sum-ii-input-array-is-sorted-two-sum-ii.java
+++ leetcode/two-sum-ii-input-array-is-sorted-two-sum-ii.java
@@ -1,13 +1,29 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left=0;
+        int right=numbers.length-1;
+        while(left<right){
+            int sum=numbers[left]+numbers[right];
+            if(sum==target){
                 return new int[]{left+1,right+1};
             }
             else if(sum>target){
                 right--;
             }
             else{
-            int sum=numbers[left]+numbers[right];
-            if(sum==target){
-        while(left<right){
-        int left=0;
-        int right=numbers.length-1;
-    public int[] twoSum(int[] numbers, int target) {
-class Solution {
+                left++;
+            }
+        }
+        return new int[] {-1,-1};
+    }
+}
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         for(int i=0;i<numbers.length;i++){
+//             if(map.containsKey(target-numbers[i])){
+//                 return new int[] {map.get(target-numbers[i])+1,i+1};
+//             }
+//             map.put(numbers[i],i);
+//         }
+//         return new int[] {-1,-1};
