41b408417ccf479585d7f9694cacb70fe9f1962c
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-18 12:20:15 +0530

    Leetcode: Trapping Rain Water

--- leetcode/trapping-rain-water-trapping-rain-water.java
+++ leetcode/trapping-rain-water-trapping-rain-water.java
@@ -5,17 +5,17 @@ class Solution {
         int max_left=height[0];
         for(int i=0;i<height.length;i++){
             max_left=Math.max(max_left,height[i]);
-        }
-        for(int i=height.length-1;i>=0;i--){
-            max_right=Math.max(max_right,height[i]);
+            leftmax[i]=max_left;
         }
         int max_right=height[height.length-1];
-        for(int i=0;i<height.length;i++){
+        for(int i=height.length-1;i>=0;i--){
+            max_right=Math.max(max_right,height[i]);
+            rightmax[i]=max_right;
+        }
         int h=0;
+        for(int i=0;i<height.length;i++){
             h+=Math.min(leftmax[i],rightmax[i])-height[i];
         }
         return h;
     }
-            leftmax[i]=max_left;
-            rightmax[i]=max_right;
 }
