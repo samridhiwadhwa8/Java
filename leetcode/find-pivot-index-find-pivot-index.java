a605a2d0efb9ee7522547b5764ea544969030a93
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-20 17:20:05 +0530

    Leetcode: Find Pivot Index

---
+++ leetcode/find-pivot-index-find-pivot-index.java
@@ -0,0 +1,20 @@
+            suffix[i]=sum;
+        }
+           sum+=nums[i];
+        for(int i=nums.length-1;i>=0;i--){
+        int[] suffix=new int[nums.length];
+       sum=0;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(prefix[i]==suffix[i])return i;
+        }
+        return -1;
+    }
+}
+            prefix[i]=sum;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+    public int pivotIndex(int[] nums) {
+        int[] prefix=new int[nums.length];
+        int sum=0;
+class Solution {
