69b3609510448028b0fb5784aafb4d7f0b64eae6
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-20 14:27:28 +0530

    Leetcode: Sort Colors

---
+++ leetcode/sort-colors-sort-colors.java
@@ -0,0 +1,24 @@
+        }
+        int idx=0;
+            while(zero!=0){
+                nums[idx++]=0;
+                zero--;
+            }
+            while(one!=0){
+                nums[idx++]=1;
+                one--;
+            }
+            while(two!=0){
+                nums[idx++]=2;
+                two--;
+            }
+            // return nums;
+    }
+}
+            if(i==0)zero++;
+            else if(i==1)one++;
+            else two++;
+        int zero=0,one=0,two=0;
+        for(int i:nums){
+class Solution {
+    public void sortColors(int[] nums) {
