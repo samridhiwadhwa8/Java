42339ebc7d3b75bc495ab1c256e3ff011601febe
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-07-22 11:22:45 +0530

    Update minimum-average-difference-minimum-average-difference.java

--- leetcode/minimum-average-difference-minimum-average-difference.java
+++ leetcode/minimum-average-difference-minimum-average-difference.java
@@ -26,4 +26,29 @@ class Solution {
         }
         return ans;
     }
+  //optimal approach
+  class Solution {
+    public int minimumAverageDifference(int[] nums) {
+        long sum=0, ls=0, rs=0;
+        int ans=0;
+        long min=Integer.MAX_VALUE;
+        for(int i: nums){
+            sum+=i;
+        }
+        for(int i=0;i<nums.length;i++){
+            ls=ls+nums[i];
+            if(i==nums.length-1){
+                rs=0;
+            }else{
+            rs=(sum-ls)/(nums.length-i-1);
+            }
+            long diff= Math.abs(ls/(i+1)- rs);
+            if(diff<min){
+                min=diff;
+                ans=i;
+            }
+        }
+        return ans;
+    }
+}
 }
