4fdb533798c2e30520055b594e964ae36ba84258
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-08-06 01:30:56 +0530

    Update remove-methods-from-project-remove-methods-from-project.java

--- leetcode/remove-methods-from-project-remove-methods-from-project.java
+++ leetcode/remove-methods-from-project-remove-methods-from-project.java
@@ -1,29 +1,46 @@
-        boolean canrem=false;
-        ArrayList<Integer> arr=new ArrayList<>();
-        for(int i=0;i<n;i++){
-                    q.add(i);
-                }
-            }
-        }
-                indeg[i]--;
-                if(!sus[i]){
-                    sus[i]=true;
-            if(!map.containsKey(p)) continue;
-            for(int i:map.get(p)){
-            int p=q.poll();
-        while(!q.isEmpty()){
-        sus[k]=true;
-        q.add(k);
-        Queue<Integer> q=new LinkedList<>();
-        }
-            indeg[v]++;
-            map.get(u).add(v);
-            map.putIfAbsent(u,new ArrayList<>());
-            int v=invocations[i][1];
-        HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
-        boolean[] sus=new boolean[n];
-        int[] indeg=new int[n];
-        for(int i=0;i<invocations.length;i++){
-            int u=invocations[i][0];
-class Solution {
-    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
+        boolean[] sus=new boolean[n];
+        int[] indeg=new int[n];
+        for(int i=0;i<invocations.length;i++){
+            int u=invocations[i][0];
+            int v=invocations[i][1];
+            map.putIfAbsent(u,new ArrayList<>());
+            map.get(u).add(v);
+            indeg[v]++;
+        }
+        Queue<Integer> q=new LinkedList<>();
+        q.add(k);
+        sus[k]=true;
+        while(!q.isEmpty()){
+            int p=q.poll();
+            if(!map.containsKey(p)) continue;
+            for(int i:map.get(p)){
+                indeg[i]--;
+                if(!sus[i]){
+                    sus[i]=true;
+                    q.add(i);
+                }
+            }
+        }
+        boolean canrem=false;
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(sus[i] && indeg[i]>0){
+                canrem=true;
+                break;
+            }
+            if(!sus[i]){
+                arr.add(i);
+            }
+        }
+        if(canrem){
+            arr=new ArrayList<>();
+            for(int i=0;i<n;i++){
+                arr.add(i);
+            }
+        }
+        return arr;
+    }
+}
