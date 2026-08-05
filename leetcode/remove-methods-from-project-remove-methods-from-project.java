d039f1f68f5fb7899beeba58e0d61f5d2e095076
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2026-08-06 01:30:18 +0530

    Leetcode: Remove Methods From Project

---
+++ leetcode/remove-methods-from-project-remove-methods-from-project.java
@@ -0,0 +1,29 @@
+        boolean canrem=false;
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=0;i<n;i++){
+                    q.add(i);
+                }
+            }
+        }
+                indeg[i]--;
+                if(!sus[i]){
+                    sus[i]=true;
+            if(!map.containsKey(p)) continue;
+            for(int i:map.get(p)){
+            int p=q.poll();
+        while(!q.isEmpty()){
+        sus[k]=true;
+        q.add(k);
+        Queue<Integer> q=new LinkedList<>();
+        }
+            indeg[v]++;
+            map.get(u).add(v);
+            map.putIfAbsent(u,new ArrayList<>());
+            int v=invocations[i][1];
+        HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
+        boolean[] sus=new boolean[n];
+        int[] indeg=new int[n];
+        for(int i=0;i<invocations.length;i++){
+            int u=invocations[i][0];
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
